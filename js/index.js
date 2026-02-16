49a920529d878d7b1f6299d266c9524ba8dc17f6
Lucas Luiz <lucassilvaluiz98@gmail.com>
2026-02-15 23:19:33 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -12,3 +12,51 @@ function proxima(){
     }
     document.querySelector("#bola"+contador).checked = true
 }
+
+let executivo = document.querySelector("#executivo")
+let familia = document.querySelector("#familia")
+let kids = document.querySelector("#kids")
+
+executivo.addEventListener("click", calc)
+familia.addEventListener("click", calc2)
+kids.addEventListener("click", calc3)
+let prato1 = 0
+let prato2 = 0
+let prato3 = 0
+let total = 0
+
+
+function calc(){
+    let valor = document.createElement("span")
+    let pedido = document.querySelector(".pedido")
+    pedido.innerHTML = ""
+    let resposta = document.querySelector(".fixo2")
+    resposta.classList.remove("esconde")
+    total = total + 22.90
+    console.log(total)
+    valor.innerHTML = `Finalizar pedido: R$${total}`
+    pedido.appendChild(valor)
+}
+
+function calc2(){
+    let valor = document.createElement("span")
+    let pedido = document.querySelector(".pedido")
+    pedido.innerHTML = ""
+    let resposta = document.querySelector(".fixo2")
+    resposta.classList.remove("esconde")
+    total = total + 45.90
+    console.log(total)
+    valor.innerHTML = `Finalizar pedido: R$${total}`
+    pedido.appendChild(valor)
+}
+function calc3(){
+    let valor = document.createElement("span")
+    let pedido = document.querySelector(".pedido")
+    pedido.innerHTML = ""
+    let resposta = document.querySelector(".fixo2")
+    resposta.classList.remove("esconde")
+    total = total + 89.90
+    console.log(total)
+    valor.innerHTML = `Finalizar pedido: R$${total}`
+    pedido.appendChild(valor)
+}
